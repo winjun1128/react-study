bd343f287c08fad21c11f85a35b1424e7b8a32b4
winjun1128 <hsj1128ty@naver.com>
2025-05-26 15:50:59 +0900

    리액트 라우터 처리, 경로별 페이지

--- src/foodmarket/pages/FoodMarket.js
+++ src/foodmarket/pages/FoodMarket.js
@@ -17,55 +17,68 @@ import foodsData from '../data/foods.Data';
 import './FoodMarket.css';
 
 import FoodCard from '../components/FoodCard';
+import { Routes, Route, Link,useNavigate } from 'react-router';
+// import { Link } from 'react-router';
+import Home from './Home';
 function FoodMarket() {
     // foodsData는 배열
     let [foods,setFoods] = useState(foodsData); 
+
+    let navigate = useNavigate();
+
     return (
         <div>
             <Navbar bg="light" data-bs-theme="light">
                 <div className="container">
                     <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        {/* <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                        <Nav.Link><Link to="/detail">detail</Link></Nav.Link>
+                        <Nav.Link><Link to="/info">info</Link></Nav.Link> */}
+                        <Nav.Link onClick={()=>{navigate("/")}}>Home</Nav.Link>
+                        <Nav.Link onClick={()=>{navigate("/detail")}}>detail</Nav.Link>
+                        <Nav.Link onClick={()=>{navigate("/info")}}>info</Nav.Link>
                     </Nav>
                 </div>
             </Navbar>
-            { /* 
-                이미지 사용
-                1)css 처리 : 이미지 경로
-                2)js  처리 : import 한 이후에 사용
-                3) public 폴더에 이미지 저장 후, 서버 경로로 접근해서 사용
 
-                src='/images/food1.jpg'
+    
 
-                *권장 방식
-                src={process.env.PUBLIC_URL+'/images/food2.jpg'}
+           
 
-                *기본 서비스 경로 루트 경로
-                서버주소:포트번호/
+            {/* react-router : 접속한 경로별로 보여줄 화면을 설정하는데 도움을 주는 라이브러리
+                                + 기타 URL 주소에 관한 각종 처리
+                 페이지 이동 처리
+                 1) Link    (=a태그)
+                    >내부 요소를 눌렸을때 페이지 이동
+                    <Link to="이동할경로"></Link>
 
-                package.json > hompage 필드 설정
-                "homepage":"/foodmarket"
+                 2) useNavigate (=location.href 비슷)
+                    함수형태로 호출해서 이동 처리
+
+                    경로처리 객체를 생성
+                    let navigate = useNavigate();
+
+                    이동할 경로를 매개변수로 전달하면서 함수 사용
+                    navigate("이동할 경로");
             */}
-            {/* <img src={banner_bg}></img> */}
-            {/* <div className='main-bg' style={{backgroundImage:'url('+banner_bg+')'}}></div> */}
-            <div className='main-bg'></div>
-            <Container>
-                <Row>
-                    {
-                        foods.map((food,index)=>{
-                            return(
-                                <Col md={4} sm={6}>
-                                    <FoodCard foods={foods} index={index} food={food}/>
-                                    {/* <FoodCard /> */}
-                                </Col>
-                            );
-                        })
-                    }
-                </Row>
-            </Container>
+            <Routes>
+                <Route path='/' element={<Home foods={foods} />}></Route>
+                <Route path='/home' element={<Home foods={foods} />}></Route>
+                <Route path='/detail' element={<h1>/detail</h1>}></Route>
+
+                {/* <Route path='/info/company' element={<h1>/info company</h1>}></Route>
+                <Route path='/info/contact' element={<h1>/info contact</h1>}></Route> */}
+                {/* 서브경로 */}
+                <Route path='/info'>
+                    <Route path='company' element={<h1>/info company</h1>}></Route>
+                    <Route path='contact' element={<h1>/info contact</h1>}></Route>
+                </Route>
+
+
+                <Route path="*" element={<div><h1>잘못된 경로입니다. 주소를 확인하세요.</h1></div>}></Route>
+
+            </Routes>
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Temp from './Temp';
-
+import {BrowserRouter} from 'react-router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     {/* <Temp/> */}
   </React.StrictMode>
 );
